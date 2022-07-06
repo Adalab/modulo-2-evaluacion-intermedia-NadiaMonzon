70d2dcf887728421b6e1d8c49822f897aa47023b
NadiaMonzon <nadiamonque99@gmail.com>
2022-07-06 09:11:51 +0100

    Add number game

---
+++ js/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const numInput = document.querySelector('.js_input');
+
+const actionBtn = document.querySelector('.js_btn');
+
+const hintText = document.querySelector('.js_hint');
+
+const tryNumber = document.querySelector('.js_num');
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }
+
+let randomNumber = getRandomNumber(100);
+
+console.log(randomNumber);
+
+
+function getHintText () {
+    const numInputValue = numInput.value;
+    
+    if (numInputValue == randomNumber) {
+        hintText.innerHTML = `Has ganado campeona!!!`
+    }
+    else if (numInputValue > randomNumber) {
+        hintText.innerHTML = `Demasiado alto`
+    }
+    else if (numInputValue < randomNumber) {
+        hintText.innerHTML = `Demasiado bajo`
+    } else if (numInputValue > 100) {
+        hintText.innerHTML = `El número debe estar entre 1 y 100`
+    }    
+    else if (numInputValue < 0) {
+        hintText.innerHTML = `El número debe estar entre 1 y 100`
+   }
+    
+};
+
+let countTrying = 0;
+
+function countText () {
+    countTrying += 1;
+    tryNumber.innerHTML = countTrying
+}
+
+function handleClick (ev) {
+    ev.preventDefault();
+    getHintText();
+    countText();
+};
+
+actionBtn.addEventListener('click', handleClick);
